ecfc4a301aad59c4e93d070e5f2b585d8b6b09b5
adelmofilho <adelmo.aguiar.filho@gmail.com>
2022-09-10 00:14:48 -0300

    fix: requiered providers on root module

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 # Configure Terraform SDK
 terraform {
-  # required_providers {
-  #   aws = {
-  #     source  = "hashicorp/aws"
-  #     version = "~> 4.0"
-  #   }
-  # }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
   backend "remote" {
     organization = "shared"
     hostname     = "app.terraform.io"
