f38cd739f13671d959de21ca58acc3fa2b63ff93
adelmofilho <adelmo.aguiar.filho@gmail.com>
2022-09-10 00:57:37 -0300

    add: outputs and layer sa-east-1

--- main.tf
+++ main.tf
@@ -24,23 +24,11 @@ module "lambda_layer_us_east_1" {
   }
 }
 
-
-# resource "aws_lambda_layer_version" "lambda_layer" {
-#   provider                 = aws.us_east_1
-#   filename                 = "/tmp/python37.zip"
-#   layer_name               = "alexa_skill_python_minimal"
-#   compatible_architectures = ["x86_64"]
-#   compatible_runtimes      = ["python3.7"]
-#   license_info             = "MIT"
-#   description              = "Python Lambda Layer with minimal packages for alex skills"
-#   source_code_hash         = filebase64sha256("/tmp/python37.zip")
-# }
-
-# resource "aws_lambda_layer_version_permission" "lambda_layer" {
-#   provider       = aws.us_east_1
-#   layer_name     = aws_lambda_layer_version.lambda_layer.layer_arn
-#   version_number = aws_lambda_layer_version.lambda_layer.version
-#   principal      = "*"
-#   action         = "lambda:GetLayerVersion"
-#   statement_id   = "alexa_skill_python_minimal"
-# }
+module "lambda_layer_sa_east_1" {
+  source         = "./modules/layer"
+  for_each       = var.python_version
+  python_version = each.value
+  providers = {
+    aws = aws.sa_east_1
+  }
+}
